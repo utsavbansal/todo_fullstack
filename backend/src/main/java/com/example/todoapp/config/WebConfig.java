a17d40897c83a010ad23d9dfb22a5bf8482364f1
utsavbansal <ubansal1996@gmail.com>
2025-12-03 18:27:36 +0530

    Added web config

---
+++ backend/src/main/java/com/example/todoapp/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.example.todoapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://192.168.29.50",
+                                "http://192.168.29.50:80",
+                                "http://192.168.29.50:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
